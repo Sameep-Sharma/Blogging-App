bb6c016ce5a4c52fd6797151fd460eb1cf1d4f06
samplex14 <sharmasameep94@gmail.com>
2026-04-19 22:27:23 +0530

    oioi

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 const express = require("express");
-const path = require('path');
-const userRoute = require('./routes/user')
-const mongoose = require("mongoose")
-const cookieParser = require('cookie-parser');
-const { checkForAuthenticationCookie } = require("./middlewares/authtentication");
+const path = require("path");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authtentication");
+
+const Blog = require("./models/blog");
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
 
 const app = express();
 const PORT = 8000;
 
-mongoose.connect('mongodb://localhost:27017/blogify').then(e=> console.log("MongoDB connected")) ;
+mongoose
+  .connect("mongodb://localhost:27017/blogify")
+  .then((e) => console.log("MongoDB connected"));
 
-
-app.set('view engine','ejs');
-app.set('views',path.resolve("./views"))
-
-app.use(express.urlencoded({extended:false}));
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(checkForAuthenticationCookie("token"))
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve('./public')))
 
-
-app.get('/',(req,res)=>{
-  res.render('home',{
-    user:req.user
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
+  res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
   });
 });
 
-app.use('/user',userRoute);
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
 
-
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
   console.log(`Server Started at ${PORT}`);
-})
+});
